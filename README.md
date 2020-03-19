92d6a58add3778a23d08cc304b5ae10f5d54294f
chenzhenan <chenzhenan@minieye.cc>
2020-03-19 15:35:31 +0800

    add download_list.py

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 收录一些自己写的有用的python脚本或代码片段
 
 [隐藏win10资源管理器中视频、图片、文档、下载、音乐、桌面、3D对象七个文件夹](remove_explorer_folder.py)
+
+[异步下载文件](download_list.py)
---
+++ download_list.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+异步下载文件列表
+"""
+
+import argparse
+import aiofiles
+import aiohttp
+import asyncio
+import os
+import traceback
+from types import SimpleNamespace
+
+
+async def fetch(context, url, savepath):
+    context.cur += 1
+    print('download {:03d} / {:03d}'.format(context.cur, context.count))
+
+    if os.path.exists(savepath):
+        print(savepath, 'exists')
+        return
+
+    timeout = aiohttp.ClientTimeout(total=2)
+    try:
+        print(f'start get: {url}')
+        async with context.session.get(url, timeout=timeout) as response:
+            data = await response.read()
+            async with aiofiles.open(savepath, 'wb') as f:
+                await f.write(data)
+
+    except Exception as e:
+        traceback.print_exc()
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('src', help="下载列表文件")
+    parser.add_argument('-p', '--parent', default='.', help="目标目录")
+    args = parser.parse_args()
+    parent = args.parent
+
+    async with aiohttp.ClientSession() as session:
+        context = SimpleNamespace(
+            session=session,
+            cur=0,
+        )
+        task_list = []
+        with open(args.src) as src_file:
+            for line in src_file.readlines():
+                url, savepath = line.split()
+                if parent != '.':
+                    savepath = os.path.join(parent, savepath)
+                task_list.append(fetch(context, url, savepath))
+
+        context.count = len(task_list)
+        sem = asyncio.Semaphore(20)
+        async with sem:
+            await asyncio.gather(*task_list)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
